880c5f8d48a659d1b0096e534ec64f82e36af10e
andrew <perepelkin.work@gmail.com>
2019-03-27 09:25:54 +0300

    build task

--- annotations/src/main/java/ru/ithub/annotations/Main.java
+++ annotations/src/main/java/ru/ithub/annotations/Main.java
@@ -2,6 +2,10 @@ package ru.ithub.annotations;
 
 public class Main {
 
+    class Inner {
+
+    }
+
     public static void main(String[] args) {
         ClassWithData data = new ClassWithData();
         data.setValue1(12);
